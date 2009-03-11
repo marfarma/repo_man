c98bf1afd16ef844de850990caa9d88656abc03d
Mark Cornick <mcornick@mcornick.com>
2009-03-11 15:41:07 -0400

    fill in path if it is not provided

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,12 @@
 class Repository < ActiveRecord::Base
   default_scope :order => 'name ASC'
 
-  validates_presence_of :name
+  validates_presence_of :name, :path
   validates_inclusion_of :scm, :in => %w(git svn), :message => 'is invalid'
+  
+  before_validation :fill_in_path
+  
+  def fill_in_path
+    self.path = self.name.to_s.parameterize.to_s if self.path.blank?
+  end
 end
--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
-  should_validate_presence_of :name
+  should_validate_presence_of :name, :path
   should_allow_values_for     :scm, 'git', 'svn'
   should_not_allow_values_for :scm, 'cvs', 'sourcesafe'
 
-  should 'default to ordering by name ASC' do
-    assert_equal 'name ASC', Repository.default_scoping.first[:find][:order]
+  context 'the Repsitory model' do
+    should 'default to ordering by name ASC' do
+      assert_equal 'name ASC', Repository.default_scoping.first[:find][:order]
+    end
+  end
+
+  context 'a Repository object' do
+    should 'fill in path on creation if it is not provided' do
+      @repository = Factory(:repository)
+      assert_equal @repository.name.to_s.parameterize.to_s, @repository.path
+    end
+    should 'not change path on creation if it is provided' do
+      @repository = Factory(:repository, :path => 'smarty_pants')
+      assert_equal 'smarty_pants', @repository.path
+    end
   end
 end
