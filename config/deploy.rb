ecc02967ae208b39459d5bda6b09faf57ebc6ba1
Mark Cornick <mcornick@mcornick.com>
2009-03-11 17:40:50 -0400

    set up deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,14 @@
-set :application, 'repo_man'
+set :application, "repo_man"
 set :use_sudo, false
-set :default_stage, 'staging'
+set :repository, "git://git.lab.viget.com/#{application}.git"
+set :deploy_to, "/var/www/#{application}/production"
 
-#set :campfire_notify, "#{application}"
+role :web, "sapporo.lab.viget.com"
+role :app, "sapporo.lab.viget.com"
+role :db,  "sapporo.lab.viget.com", :primary => true
+
+set :user, "apache"
+
+set :scm, "git"
+set :branch, "master"
+set :checkout, "export"
