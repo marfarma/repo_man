e9a06406ef63ade47d34a3bca3a3c17f6be7b2bb
Mark Cornick <mcornick@mcornick.com>
2009-03-11 15:44:48 -0400

    path must be unique

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -2,6 +2,7 @@ class Repository < ActiveRecord::Base
   default_scope :order => 'name ASC'
 
   validates_presence_of :name, :path
+  validates_uniqueness_of :path
   validates_inclusion_of :scm, :in => %w(git svn), :message => 'is invalid'
   
   before_validation :fill_in_path
--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class RepositoryTest < ActiveSupport::TestCase
-  should_validate_presence_of :name, :path
-  should_allow_values_for     :scm, 'git', 'svn'
-  should_not_allow_values_for :scm, 'cvs', 'sourcesafe'
+  should_validate_presence_of   :name, :path
+  should_allow_values_for       :scm, 'git', 'svn'
+  should_not_allow_values_for   :scm, 'cvs', 'sourcesafe'
 
   context 'the Repsitory model' do
     should 'default to ordering by name ASC' do
@@ -12,10 +12,16 @@ class RepositoryTest < ActiveSupport::TestCase
   end
 
   context 'a Repository object' do
-    should 'fill in path on creation if it is not provided' do
+    setup do
       @repository = Factory(:repository)
+    end
+    
+    should_validate_uniqueness_of :path
+
+    should 'fill in path on creation if it is not provided' do
       assert_equal @repository.name.to_s.parameterize.to_s, @repository.path
     end
+
     should 'not change path on creation if it is provided' do
       @repository = Factory(:repository, :path => 'smarty_pants')
       assert_equal 'smarty_pants', @repository.path
