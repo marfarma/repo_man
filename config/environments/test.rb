5df2538f053844b3b4781ea96145e45a9079f9e4
Mark Cornick <mcornick@mcornick.com>
2009-04-24 17:20:03 -0400

    add cucumber features and fix flash

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,8 @@ config.action_mailer.delivery_method = :test
 config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com', :version => '~> 2.10'
 config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '~> 1.2'
 config.gem 'mocha', :version => '~> 0.9.2'
+config.gem 'webrat', :version => '>= 0.4.4'
+config.gem 'cucumber', :version => '>= 0.3.0'
 
 require 'factory_girl'
 require 'mocha'
