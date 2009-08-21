38d69620de11746177d75805e88219822d7ff068
Mark Cornick <mcornick@gmail.com>
2009-08-21 16:54:01 -0400

    fix deprecation in unit tests

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -10,15 +10,23 @@ class RepositoryTest < ActiveSupport::TestCase
   context 'a Repository object' do
     setup do
       Scm.stubs(:create).returns(true)
-      @repository = Factory(:repository)
     end
     should_allow_mass_assignment_of     :name, :scm
     should_not_allow_mass_assignment_of :path
     should_validate_presence_of         :name
-    should_validate_uniqueness_of       :path
     Scm::SUPPORTED_SCM.each { |s| should_allow_values_for :scm, s }
     should_not_allow_values_for         :scm, 'cvs', 'sourcesafe'
+  end
+
+  context 'a specific Repository object' do
+    setup do
+      Scm.stubs(:create).returns(true)
+      @repository = Factory(:repository)
+    end
+    subject { @repository }
+    should_validate_uniqueness_of :path
     should 'have a slug' do
+      @repository = Factory(:repository)
       assert_equal @repository.name.to_s.parameterize.to_s, @repository.slug
     end
   end
