abbbf296e3a06657a39a272efd049c1f936cf181
Mark Cornick <mcornick@gmail.com>
2009-08-21 16:53:46 -0400

    update gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,10 +27,6 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com', :version => '~> 2.10'
-config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :version => '~> 1.2'
-config.gem 'mocha', :version => '~> 0.9.2'
-
-require 'factory_girl'
-require 'mocha'
-begin require 'redgreen'; rescue LoadError; end
+config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
+config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+config.gem 'mocha'
